be59702066ff855bff8f7b1baa9a6b2d39e9291f
Edvin Jakobsson <edvinjakobsson5@gmail.com>
2019-02-21 15:28:46 +0100

    CNN for 20news dataset done. CNN for essays started.

---
+++ CNN_for_essays.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.text import text_to_word_sequence
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.layers import Dense, Input, GlobalMaxPooling1D
+from keras.layers import Conv1D, MaxPooling1D, Embedding
+from keras.models import Model
+from keras.initializers import Constant
+
+BASE_DIR = r"C:\Users\Edvin\Projects\CNN-Master"
+GLOVE_DIR = os.path.join(BASE_DIR, 'glove.6B')
+TEXT_DATA_DIR = os.path.join(BASE_DIR, '20_newsgroup')
+
+MAX_SEQUENCE_LENGTH = 1000
+MAX_NUM_WORDS = 100000
+EMBEDDING_DIM = 100
+VALIDATION_SPLIT = 0.2
+
+
+print('Indexing word vectors.')
+
+embeddings_index = {}
+counter = 0
+max_words = -2
+f = open(r"C:\Users\Edvin\Projects\CNN-Master\glove.6B.100d.txt", encoding="utf8")
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+    counter += 1
+    if counter == max_words:
+        break;
+f.close()
+
+print('Found %s word vectors.' % len(embeddings_index))
+
+
+
+
+
+print('Processing text dataset')
+
+texts = []  # list of text samples
+labels_index = {}  # dictionary mapping label name to numeric id
+labels = []  # list of label ids
+
+i = 0
+for name in sorted(os.listdir(TEXT_DATA_DIR)):
+    if i == 2:
+        break;
+        i += 1
+    path = os.path.join(TEXT_DATA_DIR, name)
+    if os.path.isdir(path):
+        label_id = len(labels_index)
+        labels_index[name] = label_id
+        for fname in sorted(os.listdir(path)):
+            if fname.isdigit():
+                fpath = os.path.join(path, fname)
+                args = {} if sys.version_info < (3,) else {'encoding': 'latin-1'}
+                with open(fpath, **args) as f:
+                    t =f.read()
+                    i = t.find('\n\n')
+                    if 0 <i:
+                        t = t[i:]
+                    texts.append(t)
+                labels.append(label_id)
+
+print('Found %s texts. ' % len(texts))
