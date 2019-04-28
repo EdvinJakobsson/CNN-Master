be12764c79ec60d9d00ba0d5a628f98c8653cf5c
Edvin Jakobsson <edvinjakobsson5@gmail.com>
2019-04-28 17:04:31 +0200

    m

--- models.py
+++ models.py
@@ -24,24 +24,20 @@ def CNN_sigmoidal_output(MAX_SEQUENCE_LENGTH, embedding_layer, layers = 2, kerne
     return model
 
 
+        #no batch normalization, dropout on embedding, GlobalMaxPooling
+def CNN_sigmoidal_output2(MAX_SEQUENCE_LENGTH, embedding_layer, layers = 2, kernels = 1, kernel_length = 1, dropout = 0):
 
-def CNN_sigmoidal_output2(MAX_SEQUENCE_LENGTH, embedding_layer, layers = 2, kernels = 1, kernel_length = 1, dense = 50, dropout = 0):
-
-    # train a 1D convnet with global maxpooling
     sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
-    embedded_sequences = embedding_layer(sequence_input)
-    x = Conv1D(kernels, kernel_length, activation='relu')(embedded_sequences)
-    for layers in range(1, layers):
-        x = MaxPooling1D(5)(x)
-        x = Conv1D(kernels, kernel_length, activation='relu')(x)
-    x = MaxPooling1D(5)(x)
-    x = dense(dense, activation='relu')(x)
-    x = Dropout(dropout)(x)
-    y = Dense(1, activation='sigmoid')(x)
+    model = Sequential()
+    model.add(embedding_layer)
+    model.add(Dropout(dropout))
+    model.add(Conv1D(kernels, kernel_length, activation='relu'))
+    model.add(MaxPooling1D(5))
+    model.add(Conv1D(kernels, kernel_length, activation='relu'))
+    model.add(GlobalMaxPooling1D())
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='mse', optimizer="rmsprop", metrics=['accuracy'])
 
-    model = Model(sequence_input, y)
-    model.compile(loss='mse',
-                  optimizer='rmsprop', metrics=['acc'])
     return model
 
 
