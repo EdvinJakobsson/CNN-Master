347116e9af556ef6fd38afe0c3f33c0ccfcc8fd7
Edvin Jakobsson <edvinjakobsson5@gmail.com>
2019-05-02 13:22:06 +0200

    m

--- test2.py
+++ test2.py
@@ -31,9 +31,9 @@ kernel_numbers = [100]
 kernel_length_number = [3]
 numbers_of_kappa_measurements = 20
 epochs_between_kappa = 10
-#dropout_numbers = [0, 0.5, 0.99]
+dropout_numbers = [0, 0.5, 0.99]
 #dropout_numbers = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.99]
-dropout_numbers = [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
+#dropout_numbers = [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
 
 essayfile = "/home/william/m18_edvin/Projects/Data/asap-aes/training_set_rel3.tsv"
 wordvectorfile = "/home/william/m18_edvin/Projects/Data/glove.6B/glove.6B.100d.txt"
